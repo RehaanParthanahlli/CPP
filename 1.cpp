d8fceb5f2524bf6189b60aae5b7b3da412911491
RehaanParthanahlli <rehaanparthanahalli@gmail.com>
2025-11-30 16:04:28 +0530

    Dawn1

---
+++ 1.cpp
@@ -0,0 +1,61 @@
+/*#include <iostream>
+using namespace std;
+int main() {
+    cout << "Hello, World!" << endl;
+    std::cout<< " This project uses the C++17 language standard version. ie if "<<"\n using name space std \n isn't used " << std::endl;
+    return 0;
+}*/
+
+/*#include "iostream"
+int main() {
+    int y=5;  // x is called as variable or identifier 
+    // OR
+    int x;
+    x=5;
+    std::cout<< x << y;
+    // no need of return 0 in modern compilers
+}*/
+
+/*#include <iostream>
+int main(){
+    //integer
+    int x=9.8;
+    //double
+    double y=9.8;
+    //string 
+    std::string z="Rehaan";
+    //char
+    char a='d';
+    //boolean
+    bool b=true;
+}*/
+
+/*#include <iostream>
+int main() {
+    const double PI=3.12;
+    std::cout<<PI;
+}*/
+
+/*#include <iostream>
+namespace x1{
+    int x=1;
+}
+int main(){
+    int x=0;
+    std::cout<<x<<x1::x;
+}
+int main(){
+    using namespace x1;
+    std::cout<<x;
+}*/
+
+/*#include <iostream>
+int main(){
+    using std::string;
+    string name="Rehaan";
+}
+int main(){
+    using namespace std;
+    int x=5;
+    cout<<x;
+}*/
